6e85e39ad93c1d1e5dc446093990da4ec7f9e6cb
richardykim <richard.y.kim4@gmail.com>
2017-03-09 04:01:08 -0500

    Add survey to file

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name= gets.chomp
+puts "How old are you?"
+age= gets.chomp.to_i
+puts "What year were you born?"
+year= gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+garlic= gets.chomp
+puts "Would you like to enroll in the company's health insurance? (yes or no)"
+insurance= gets.chomp
