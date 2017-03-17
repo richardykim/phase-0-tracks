86f48177351f22d97d57d655776936b9605fd1a7
richardykim <richard.y.kim4@gmail.com>
2017-03-17 03:16:40 -0400

    Make nested restaurant menu

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,2 +1,52 @@
+
 restaurant_menu = {
-	}
+  breakfast: {
+    meals: {
+      scrambled_eggs: "$3",
+      pancakes: "$5",
+      toast: "$2"
+    },
+    drinks: {
+      orange_juice: "$2",
+      milk: "$2",
+      fruit_smoothie: "$5",
+      coffee: "$3"
+    },
+    types_of_coffee: [
+      "decaf",
+      "hazelnut",
+      "dark roast"
+      ]
+  },
+  lunch: {
+    meals: {
+      hamburger: "$8",
+      salad: "$6",
+      pizza: "$5",
+      spaghetti: "$8"
+    },
+    drinks: {
+      soda: "$2",
+      sparkling_water: "$3",
+      juice: "$2"
+    }
+  },
+  dinner: {
+    meals:{
+      steak: "$20",
+      lobster: "$25",
+      pork_chop: "$19"
+    },
+    drinks: [
+      "wine",
+      "beer",
+      "liquor"
+      ],
+    beer_selection: {
+      green_moon: "$7",
+      queens_lager: "$6",
+      doors_light: "$5"
+    }
+  }
+  
+}
