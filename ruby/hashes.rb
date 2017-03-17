3e70e4f5ed3401314cb0a0dcf04d6dd97ce8ea27
richardykim <richard.y.kim4@gmail.com>
2017-03-16 21:26:45 -0400

    Ask user if changes are necessary

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,3 +20,34 @@ information[:name]= name
 information[:age]= age
 information[:children]= children
 information[:theme]= decor_theme
+
+p information
+
+puts "Of the following options, do any corrections need to be made? 
+Name, age, number of children, decor theme. 
+(type 'none' if no changes are necessary)"
+answer = gets.chomp
+
+if answer == "name"
+  puts "What is your name?"
+  name = gets.chomp
+  information[:name] = name
+elsif answer == "age"
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  information[:age] = age
+elsif answer == "number of children"
+  puts "How many children do you have?"
+  children = gets.chomp.to_i
+  information[:children] = children
+elsif answer == "decor theme"
+  puts "What would you like your decor theme to be?"
+  decor_theme = gets.chomp
+  information[:theme] = decor_theme
+elsif answer == "none"
+else
+  puts "I am sorry that is not an option"
+end
+
+puts "Thank you for your submission."
+p information
