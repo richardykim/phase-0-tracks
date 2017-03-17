f9fcd28490ed7645a38a77118c82e282bd4f9968
richardykim <richard.y.kim4@gmail.com>
2017-03-16 21:25:08 -0400

    Ask user for input and add input to hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,3 +3,20 @@
 # I will then print the hash.
 # I will give them the option to change one of their previous answers.
 # I will print the hash one last time.
+
+information = {}
+
+puts "Welcome to the design helper"
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "How many children do you have?"
+children = gets.chomp.to_i
+puts "What would you like your decor theme to be?"
+decor_theme = gets.chomp
+
+information[:name]= name
+information[:age]= age
+information[:children]= children
+information[:theme]= decor_theme
