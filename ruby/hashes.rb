21b5c1783846a10cfd7f0e4bde171770256457e5
richardykim <richard.y.kim4@gmail.com>
2017-03-16 21:24:36 -0400

    Add pseudocode for assignment

---
+++ ruby/hashes.rb
@@ -0,0 +1,5 @@
+# I will create an empty hash and ask the user for their information.
+# After collecting their input, I will transfer their answers into a hash.
+# I will then print the hash.
+# I will give them the option to change one of their previous answers.
+# I will print the hash one last time.
