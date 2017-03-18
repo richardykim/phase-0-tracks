0d687332eaf9c44d498ca42e0450e2026b374275
richardykim <richard.y.kim4@gmail.com>
2017-03-18 14:31:53 -0400

    Add pseudocode to file

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,4 @@
+# I will ask the user for their first name and last name
+# I will take their names and swap the first and the last
+# I will then take the vowels of the names and move them over to the next vowel
+
