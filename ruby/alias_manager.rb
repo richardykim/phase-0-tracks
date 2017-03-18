5e02db432bec28c765c3e71bfb89c9bebd92ea4e
richardykim <richard.y.kim4@gmail.com>
2017-03-18 15:16:43 -0400

    Create method that takes name and makes spy name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,9 +3,19 @@
 # I will then take the vowels of the names and move them over to the next vowel
 
 
-puts "What is your first name?"
-first_name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
-
-reverse_name = last_name + " " + first_name
+def spy_name(first_name,last_name)
+  reverse_name = last_name + " " + first_name
+  vowels = ["a", "e", "i", "o", "u"]
+  rotated_vowels = vowels.rotate 1
+  rev_characters = reverse_name.split('')
+  
+  str_new = rev_characters.map do |char|
+    if vowels.include?(char)
+      vowels.rotate(1)[vowels.index(char)]
+    else
+      char
+    end
+  end
+  str_new.join
+  
+end
