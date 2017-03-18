5169a229b8e0ce9c5ad473270366cf30608abc43
richardykim <richard.y.kim4@gmail.com>
2017-03-18 14:34:12 -0400

    Add code to take name input and print reverse name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,3 +2,10 @@
 # I will take their names and swap the first and the last
 # I will then take the vowels of the names and move them over to the next vowel
 
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+reverse_name = last_name + " " + first_name
