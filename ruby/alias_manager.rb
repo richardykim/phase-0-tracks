546d61e920ba77f6196d6c5647e9450c61f2f48d
richardykim <richard.y.kim4@gmail.com>
2017-03-18 15:33:23 -0400

    Add consonant value rotation

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,18 +4,23 @@
 
 
 def spy_name(first_name,last_name)
-  reverse_name = last_name + " " + first_name
+  reverse_name = (last_name + " " + first_name).downcase
   vowels = ["a", "e", "i", "o", "u"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
+  "s", "t", "v", "w", "x", "y", "z"]
   rotated_vowels = vowels.rotate 1
+  rotated_consonants = consonants.rotate 1
   rev_characters = reverse_name.split('')
   
   str_new = rev_characters.map do |char|
     if vowels.include?(char)
       vowels.rotate(1)[vowels.index(char)]
+    elsif consonants.include?(char)
+      consonants.rotate(1)[consonants.index(char)]
     else
       char
     end
   end
-  str_new.join
+  full_spyname = str_new.join
   
 end
