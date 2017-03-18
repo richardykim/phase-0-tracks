1c03d0ec5ecd53a0f49b068d18b81fd0170fe981
richardykim <richard.y.kim4@gmail.com>
2017-03-18 15:55:11 -0400

    Add interface and loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,25 +2,38 @@
 # I will take their names and swap the first and the last
 # I will then take the vowels of the names and move them over to the next vowel
 
-
-def spy_name(first_name,last_name)
-  reverse_name = (last_name + " " + first_name).downcase
-  vowels = ["a", "e", "i", "o", "u"]
-  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
-  "s", "t", "v", "w", "x", "y", "z"]
-  rotated_vowels = vowels.rotate 1
-  rotated_consonants = consonants.rotate 1
-  rev_characters = reverse_name.split('')
+answer=""
+until answer == "quit"
+  puts "What is your first name?"
+  first_name = gets.chomp
+  puts "What is your last name?"
+  last_name = gets.chomp
   
-  str_new = rev_characters.map do |char|
-    if vowels.include?(char)
-      vowels.rotate(1)[vowels.index(char)]
-    elsif consonants.include?(char)
-      consonants.rotate(1)[consonants.index(char)]
-    else
-      char
+  def spy_name(first_name,last_name)
+    reverse_name = (last_name + " " + first_name).downcase
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
+    "s", "t", "v", "w", "x", "y", "z"]
+    rotated_vowels = vowels.rotate 1
+    rotated_consonants = consonants.rotate 1
+    rev_characters = reverse_name.split('')
+    
+    str_new = rev_characters.map do |char|
+      if vowels.include?(char)
+        vowels.rotate(1)[vowels.index(char)]
+      elsif consonants.include?(char)
+        consonants.rotate(1)[consonants.index(char)]
+      else
+        char
+      end
     end
+    full_spyname = str_new.join
+    
   end
-  full_spyname = str_new.join
   
+  puts spy_name(first_name,last_name)
+  
+  puts "Would you like to try again? (type 'yes' to continue and 'quit' to stop)"
+  answer = gets.chomp
+
 end
