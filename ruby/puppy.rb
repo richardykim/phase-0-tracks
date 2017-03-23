ea7bc931e0ae286feaa1347cabdfceef80df8a9a
richardykim <richard.y.kim4@gmail.com>
2017-03-23 01:58:27 -0400

    Add puppy class in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# Puppies
+
+## SPECIES -----------------------------
+## Golden Retriever
+
+## CHARACTERISTICS ---------------------
+## Color: Gold, light brown
+## Life span: 10-12 years
+## Height: Female: 20-22 inches, Male 22-24 inches
+## Weight: Female: 55-75 lbs, Male 65-75 lbs
+
+## BEHAVIOR ----------------------------
+## Reliable
+## Kind
+## Friendly
+## Loyal
+## Intelligent
