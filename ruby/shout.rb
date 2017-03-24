7aa435f7c3bf1ef02b70025f0fe3b952db146c6a
richardykim <richard.y.kim4@gmail.com>
2017-03-24 01:49:26 -0400

    Add class bird and plane to flight file

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    puts words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+  	puts words + "!!!" + ":)"
+  end
 end
+
+
+
+Shout.yell_angrily("Hello")
+Shout.yelling_happily("Hello")
