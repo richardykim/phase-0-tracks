b8eaf566ae2d0ed16f64c62f645d2d3272590ebe
richardykim <richard.y.kim4@gmail.com>
2017-03-23 23:36:53 -0400

    Add get_mad_at and birthday methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,45 +1,46 @@
 
 class Santa
-	attr_reader :ethnicity
+	attr_reader :ethnicity, :age
 	attr_accessor :gender
 
-	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	age = 0
-
-	def speak(words)
+	def speak()
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
- 	def celebrate_birthday()
- 		new_age = age + 1
- 		new_age
+ 	def celebrate_birthday(age = 0)
+ 		new_age = age.to_i + 1
+ 		puts "Santa is now #{new_age}"
  	end
- 	def get_mad_at(reindeer)
- 		reindeer_ranking
+ 	def get_mad_at(reindeer)	
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		new_reindeer_ranking = reindeer_ranking.insert(8, reindeer_ranking.delete_at(reindeer_ranking.index(reindeer)))
+		puts new_reindeer_ranking
  	end
-	def initialize(gender, ethnicity, horoscope, birth_place)
+	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		@horoscope = horoscope
-		@birth_place = birth_place
-		puts "Initializing Santa instance #{gender}, #{ethnicity}, #{horoscope}, #{birth_place}"
+		puts "Initializing Santa instance #{gender}, #{ethnicity}"
 	end
 end
 
 ## DRIVER CODE
 
-# santa = Santa.new("Male", "White", "leo", "Texas")
+santa = Santa.new("Male", "White")
+santa.get_mad_at("Dasher")
+
+# santa.celebrate_birthday
 # santa.speak("hello")
 # santa.eat_milk_and_cookies("chocolate-chip")
 
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_horoscopes = ["Leo", "Scorpio", "Aries", "Taurus", "Cancer", "Gemini", "Virgo", "Libra"]
-example_birth_place = ["Rhode Island", "New Jersey", "Mexico", "China", "Paris", "Italy", "Japan", "California"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i], example_horoscopes[i], example_birth_place[i])
-end
+# santas = []
+# example_genders = ["male", "female", "bigender", "male in the morning, femaile at night", "3/4 female, 1/4 male", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+
+
