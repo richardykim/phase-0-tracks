a57d4a7e0e102e45862aa7a32fdb319240e164b2
richardykim <richard.y.kim4@gmail.com>
2017-03-24 01:53:36 -0400

    Add pseudocode to shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,10 @@
+## Old code
+## Create module called "Shout"
+## Create method called "yell_angrily" in module that takes in a string and adds exclamation marks and a sad face
+## It will return a new string
+## Create a new method called "yelling_happily" that takes in a string and adds exclamation marks with a happy face
+## It will return a new string
+
 module Shout
   def self.yell_angrily(words)
     puts words + "!!!" + " :("
