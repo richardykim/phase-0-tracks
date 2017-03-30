9bf5f7c2ddf0dac25d97cf1dd73db9d62065281f
richardykim <richard.y.kim4@gmail.com>
2017-03-30 00:16:39 -0400

    Add method and driver code

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,11 @@
 // I will create a function that takes in a string as a parameter. This function will take each letter and reposition it so that the result is the reverse of the inputted strung.
+
+
+function reverse(word){
+	return word
+}
+
+
+
+//Driver Code
+console.log(reverse("hello"));
