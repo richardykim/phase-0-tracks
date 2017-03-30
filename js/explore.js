68b34f704b3fe62e68db483a4aa7c356b2e0fe87
richardykim <richard.y.kim4@gmail.com>
2017-03-30 01:03:56 -0400

    Create function that will reverse string input

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,15 @@
 // I will create a function that takes in a string as a parameter. This function will take each letter and reposition it so that the result is the reverse of the inputted strung.
 
-
 function reverse(word){
-	return word
+	revWord = "";
+	for (var i = word.length - 1; i >= 0; i=i-1 ){
+	  revWord = revWord + word[i];
+	}
+	
+	
+	
+	return revWord
 }
 
 
-
-//Driver Code
 console.log(reverse("hello"));
