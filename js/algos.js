c56eef14a6aed6541bd437a9ebf7861705338e51
richardykim <richard.y.kim4@gmail.com>
2017-04-02 14:52:47 -0400

    Add function

--- js/algos.js
+++ js/algos.js
@@ -1,1 +1,7 @@
 // I will make a function that takes an array of words or phrases. I will take the length of each word or phrase and return the one with the greatest length.
+
+
+function longestWord(array) {
+
+	
+}
