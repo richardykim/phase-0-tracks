166ca58aff5c00ad3b76236e1a56fd514ac2962d
richardykim <richard.y.kim4@gmail.com>
2017-04-02 19:59:12 -0400

    Add function to get longest word

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,24 @@
 // I will make a function that takes an array of words or phrases. I will take the length of each word or phrase and return the one with the greatest length.
 
 
-function longestWord(array) {
+var list = ["word", "longer word", "longest word"];
 
-	
+function longestWord(array) {
+  var longestLength = 0;
+  var longestWordCount = "";
+  for (var i = 0; i <= array.length-1; i++){
+    if (array[i].length > longestLength){
+      longestLength = array[i].length;
+      longestWordCount = array[i];
+    }
+  }
+    
+  console.log(longestWordCount);
 }
+
+longestWord(list)
+
+// console.log (list.length);
+var anotherList = ["Lion", "Tiger", "Elephant", "Giraffe", "Alligator"]
+
+longestWord(anotherList)
